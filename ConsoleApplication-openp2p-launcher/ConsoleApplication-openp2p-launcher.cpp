8172c0a8c763324be1c2739b2b36a4633479fe30
Guailoudou <guailoudou@outlook.com>
2023-11-06 11:38:24 +0800

    updata

--- ConsoleApplication-openp2p-launcher/ConsoleApplication-openp2p-launcher.cpp
+++ ConsoleApplication-openp2p-launcher/ConsoleApplication-openp2p-launcher.cpp
@@ -169,6 +169,7 @@ int main(int argc,char* argv[])
             Sleep(2000);
             std::stringstream ss, ss2;
             Clink(uuid,DstPort, argv[0]);
+            std::cout << "> 已为当前连接创建快捷方式 <" << std::endl;
             play1(DstPort, uuid, myuuid);
             while (true)
             {
@@ -279,104 +280,6 @@ void Clink(std::string uuid, int DstPort,char* paths) {
     // 释放 COM 库
     CoUninitialize();
 }
-//BOOL CreateFileShortcut(
-//
-//    LPCSTR lpszFileName,
-//    LPCSTR lpszLnkFileDir,
-//    LPCSTR lpszLnkFileName,
-//    LPCSTR lpszWorkDir,
-//    WORD wHotkey,
-//    LPCTSTR lpszDescription,
-//    int iShowCmd)
-//{
-//    if (lpszLnkFileDir == NULL)
-//        return FALSE;
-//
-//
-//    HRESULT hr;
-//    IShellLink* pLink;  //IShellLink对象指针
-//    IPersistFile* ppf; //IPersisFil对象指针
-//
-//
-//    //创建IShellLink对象
-//    hr = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, IID_IShellLink, (void**)&pLink);
-//    if (FAILED(hr))
-//        return FALSE;
-//
-//
-//    //从IShellLink对象中获取IPersistFile接口
-//    hr = pLink->QueryInterface(IID_IPersistFile, (void**)&ppf);
-//    if (FAILED(hr))
-//    {
-//        pLink->Release();
-//        return FALSE;
-//    }
-//
-//
-//    //目标
-//    if (lpszFileName == NULL)
-//        pLink->SetPath(_pgmptr);
-//    else
-//        pLink->SetPath(lpszFileName);
-//
-//
-//    //工作目录
-//    if (lpszWorkDir != NULL)
-//        pLink->SetWorkingDirectory(lpszWorkDir);
-//
-//
-//    //快捷键
-//    if (wHotkey != 0)
-//        pLink->SetHotkey(wHotkey);
-//
-//
-//    //备注
-//    if (lpszDescription != NULL)
-//        pLink->SetDescription(lpszDescription);
-//
-//
-//    //显示方式
-//    pLink->SetShowCmd(iShowCmd);
-//
-//    //快捷方式的路径 + 名称
-//    char szBuffer[MAX_PATH];
-//    if (lpszLnkFileName != NULL) //指定了快捷方式的名称
-//        sprintf(szBuffer, "%s\\%s", lpszLnkFileDir, lpszLnkFileName);
-//    else
-//    {
-//        //没有指定名称，就从取指定文件的文件名作为快捷方式名称。
-//        const char* pstr;
-//        if (lpszFileName != NULL)
-//            pstr = strrchr(lpszFileName, '\\');
-//        else
-//            pstr = strrchr(_pgmptr, '\\');
-//
-//
-//        if (pstr == NULL)
-//        {
-//            ppf->Release();
-//            pLink->Release();
-//            return FALSE;
-//        }
-//        //注意后缀名要从.exe改为.lnk
-//        sprintf(szBuffer, "%s\\%s", lpszLnkFileDir, pstr);
-//        int nLen = strlen(szBuffer);
-//        szBuffer[nLen - 3] = 'l';
-//        szBuffer[nLen - 2] = 'n';
-//        szBuffer[nLen - 1] = 'k';
-//    }
-//    //保存快捷方式到指定目录下
-//    WCHAR  wsz[MAX_PATH];  //定义Unicode字符串
-//    MultiByteToWideChar(CP_ACP, 0, szBuffer, -1, wsz, MAX_PATH);
-//
-//
-//    hr = ppf->Save(wsz, TRUE);
-//
-//
-//    ppf->Release();
-//    pLink->Release();
-//    return SUCCEEDED(hr);
-//}
 void openp2p() {
     std::cout << "程序开始运行了，请稍等片刻" << std::endl;
     system("bin\\openp2p.exe");
